215ae7dc726f46a816781d285b80f710c024abe3
vinjar <vinjar.dev@gmail.com>
2024-10-06 12:07:48 +0200

    ferdig med ajax oppgave second.html

--- script.js
+++ script.js
@@ -1,32 +1,73 @@
-let page = 1
-const postPerpage = 9
+let maxPosts = 18; 
+const absoluteMax = 90; 
+const postsPerPage = 3;
+let page = 1; 
+let isLoading = false;
+
 
 async function getData() {
-    const url = "https://jsonplaceholder.typicode.com/posts"
+   
+    const url = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${postsPerPage}`;
+
     try {
-        const respone = await fetch(url)
-        if (!respone.ok) {
-            throw new Error(`Response status: ${respone.status}`)
+        isLoading = true
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
         }
-        
-        const data = await respone.json()
-        console.log(data)
-        
-        page++
-        displayPost(data)
+
+        const data = await response.json();
+
+        displayPosts(data);
+        page++; 
+        isLoading = false
 
     } catch (error) {
-        console.log(error)
+        console.error("Failed to fetch data:", error);
     }
 }
 
-function displayPost(posts) {
-    const container = document.getElementById("post")
-    
+function displayPosts(posts) {
+    const container = document.getElementById("post-container"); // gets id from only element in body
+    let row;
+
+    posts.forEach((post, index) => {
+        if ((index) % postsPerPage === 0) {
+            row = document.createElement("div"); // create a new row, if row is done
+            row.classList.add("row");            // make 3 "rows" see css
+        }
+
+        const col = document.createElement("div"); 
+        col.classList.add("col", "post");          // give it col and post properties
+        col.innerHTML = `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>`;
+        row.appendChild(col);
+
+        if ((index + 1) % postsPerPage === 0) {
+            container.appendChild(row);
+            }
+        
+    });
 }
 
 document.addEventListener("scroll",()=> {
-    getData()
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight - 10 && !isLoading && maxPosts <= absoluteMax) {
+        maxPosts += 3;
+        getData();
+        page++
+    }
+
 })
 
-getData()
+// initial loading
+for (let i = 0; i < 2; i++) {
+    getData()
+    page++
+}
+
